19825c7275a1b7d84df4c789dadb3e2ea80af423
ICEBergJQ <salahsk52@gmail.com>
2025-08-06 12:48:19 +0100

    adding main func adding basic usage text

--- chaikin/src/main.rs
+++ chaikin/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("Interactive Points Example")]
+async fn main() {
+        draw_text(
+            "Left click to add points | R to reset | ESC to exit",
+            20.0,
+            20.0,
+            25.0,
+            GREEN,
+        );
 }
