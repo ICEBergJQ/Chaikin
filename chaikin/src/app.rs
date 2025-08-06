9c9bcef386409f3bb88e4a68f9122c33a4f9d3a4
ICEBergJQ <salahsk52@gmail.com>
2025-08-06 15:20:52 +0100

    adding draw_ui func

--- chaikin/src/app.rs
+++ chaikin/src/app.rs
@@ -35,3 +35,25 @@ impl App {
         *self = App::new();
     }
 }
+
+
+pub fn draw_ui(app: &App) {
+      if !app.start_animation{
+            draw_text(
+                "Left click to add points | R to reset | ESC to exit",
+                20.0,
+                20.0,
+                25.0,
+                GREEN,
+            );
+        }
+
+        draw_text(
+            &format!("init Points: {}", app.default_points.len()),
+            20.0,
+            50.0,
+            20.0,
+            WHITE,
+        );
+        draw_text(&format!("steps: {}", app.steps), 20.0, 80.0, 20.0, WHITE);
+}
