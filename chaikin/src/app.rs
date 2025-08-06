4387d28ea35fac2ec473d9d3b408db589c1869b5
ICEBergJQ <salahsk52@gmail.com>
2025-08-06 15:20:14 +0100

    adding app struct and impl

--- chaikin/src/app.rs
+++ chaikin/src/app.rs
@@ -9,3 +9,29 @@ impl Point {
         Self { x, y }
     }   
 }
+
+pub struct App {
+    pub default_points: Vec<Point>,
+    pub chaikin_points: Vec<Point>,
+    pub start_animation: bool,
+    pub steps: u32,
+}
+
+impl App {
+    pub fn new() -> Self {
+          Self {
+            default_points: Vec::new(),
+            chaikin_points: Vec::new(),
+            start_animation: false,
+            steps: 0,
+        }
+    }
+
+     pub fn new_point(&mut self, x: f32, y: f32) {
+        self.default_points.push(Point::new(x, y))
+    }
+
+    pub fn reset(&mut self) {
+        *self = App::new();
+    }
+}
